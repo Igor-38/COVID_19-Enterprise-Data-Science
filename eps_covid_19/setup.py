9f3ac387e3c876993fcbda5b0de0ab4095d0a91e
Igor-38 <anubhavms2021@gmail.com>
2022-06-29 22:01:25 +0200

    Initial commit

---
+++ eps_covid_19/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Coivid 19 dashboard',
+    author='Anubhav_routray',
+    license='',
+)
